87485808f79e0eebffbf16c12d162465047e38ca
bneuhausz <bneuhausz@gmail.com>
2025-10-01 10:39:44 +0200

    added amqp sender

---
+++ dotnet-amqp-messaging/AmqpSender.cs
@@ -0,0 +1,36 @@
+﻿using Amqp;
+
+namespace dotnet_amqp_messaging;
+public class AmqpSender
+{
+    public async Task SendMessageAsync(string messageBody)
+    {
+        Connection? connection = null;
+        try
+        {
+            Address address = new Address("localhost", 5672, "admin", "admin", scheme: "amqp");
+            connection = await Connection.Factory.CreateAsync(address);
+            var session = new Session(connection);
+            var sender = new SenderLink(session, "demo-sender", "demo-target");
+
+            var message = new Message(messageBody);
+            await sender.SendAsync(message);
+
+            Console.WriteLine("Message sent to AMQP topic");
+
+            await sender.CloseAsync();
+            await session.CloseAsync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            if (connection != null)
+            {
+                await connection.CloseAsync();
+            }
+        }
+    }
+}
--- dotnet-amqp-messaging/Program.cs
+++ dotnet-amqp-messaging/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using dotnet_amqp_messaging;
+
+var sender = new AmqpSender();
+await sender.SendMessageAsync("demo message");
