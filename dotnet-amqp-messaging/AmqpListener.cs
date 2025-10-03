74ef93dd930ba17fc72839b5fbff56aced428620
bneuhausz <bneuhausz@gmail.com>
2025-10-03 11:43:18 +0200

    added receiver

---
+++ dotnet-amqp-messaging/AmqpListener.cs
@@ -0,0 +1,47 @@
+﻿using Amqp;
+
+namespace dotnet_amqp_messaging;
+public class AmqpListener
+{
+    public async Task StartListeningAsync(CancellationToken cancellationToken)
+    {
+        var brokerUrl = "amqp://admin:admin@localhost:5672";
+        var addressName = "demo-target";
+
+        Connection? connection = null;
+        try
+        {
+            connection = await Connection.Factory.CreateAsync(new Address(brokerUrl));
+            var session = new Session(connection);
+            var receiver = new ReceiverLink(session, "demo-receiver", addressName);
+
+            Console.WriteLine($"Receiver attached to address '{addressName}'. Waiting for messages...");
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var message = await receiver.ReceiveAsync(TimeSpan.FromSeconds(1));
+
+                if (message != null)
+                {
+                    Console.WriteLine($"Received Message: '{message.Body}'");
+                    receiver.Accept(message);
+                }
+            }
+
+            await receiver.CloseAsync();
+            await session.CloseAsync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        finally
+        {
+            if (connection != null && !connection.IsClosed)
+            {
+                await connection.CloseAsync();
+                Console.WriteLine("Connection closed.");
+            }
+        }
+    }
+}
--- dotnet-amqp-messaging/Program.cs
+++ dotnet-amqp-messaging/Program.cs
@@ -1,4 +1,20 @@
 ﻿using dotnet_amqp_messaging;
 
+Console.WriteLine("Demo started. Press Ctrl+C to exit.");
+
 var sender = new AmqpSender();
 await sender.SendMessageAsync("demo message");
+
+var cts = new CancellationTokenSource();
+
+Console.CancelKeyPress += (sender, eventArgs) =>
+{
+    eventArgs.Cancel = true;
+    cts.Cancel();
+    Console.WriteLine("Shutting down...");
+};
+
+var listener = new AmqpListener();
+await listener.StartListeningAsync(cts.Token);
+
+Console.WriteLine("Application has shut down.");
